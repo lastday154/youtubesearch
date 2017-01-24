f632556abe081e032f4f55fd94d0570687962e03
Steve Tuan <vothuong197@gmail.com>
2017-01-24 22:46:49 +0800

    add next page and prev page

--- js/script.js
+++ js/script.js
@@ -45,9 +45,63 @@ function search(){
 			type: 'video',
 			key: 'AIzaSyCho-qt9KljKcSiN1YOT2OO03fQcz1o23U'
 		}, function(data) {
+			var nextPageToken = data.nextPageToken;
+			var prevPageToken = data.prevPageToken;
 			$.each(data.items, function(i, item){
-				console.log(item);
+				// Get output
+				var output = getOutput(item);
+
+				// Display results
+				$('#results').append(output);
+
 			});
+
+			var buttons = getButtons(prevPageToken, nextPageToken);
+
+			$('#buttons').append(buttons);
 		}
 	);
+}
+
+// Build the buttons
+function getButtons(prevPageToken, nextPageToken) {
+	if (!prevPageToken) {
+		var btnoutput = '<div class="button-container">' +
+			'<button id="next-button" class="paging-button" data-token="'+nextPageToken+'"' +
+			 'data-query="'+q+'"' + 'onclick="nextPage();">Next Page </button></div>';								
+	} else {
+		var btnoutput = '<div class="button-container">' +
+			'<button id="next-button" class="paging-button" data-token="'+prevPageToken+'"' +
+			 'data-query="'+q+'"' + 'onclick="prevPage();">Prev Page </button>' + 
+			'<button id="next-button" class="paging-button" data-token="'+nextPageToken+'"' +
+			 'data-query="'+q+'"' + 'onclick="nextPage();">Next Page </button></div>';	
+	}
+	return btnoutput;
+}
+
+// Build output
+function getOutput(item) {
+	console.log(item);
+	var videoId = item.id.videoId;
+	var title = item.snippet.title;
+	var description = item.snippet.description;
+	var thumb = item.snippet.thumbnails.high.url;
+	var chanelTitle = item.snippet.channelTitle;
+	var videoDate = item.snippet.publishedAt;
+
+	// Build output string
+	var output = '<li>' +
+	'<div class="list-left">' +
+	'<img src="'+thumb+'">' +
+	'</div>'+
+	'<div class="list-right">' +
+	'<h3>'+ title + '</h3>'  + 
+	'<small>By <span class="cTitle">' + chanelTitle + '</span> on ' + videoDate + '</small>' +
+	'<p>' + description+'</p>' +
+	'</div>' +
+	'</li>' +
+	'<div class="clearfix"></div>' +
+	'';
+
+	return output;
 }
